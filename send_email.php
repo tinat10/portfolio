0a210f9d7717f1f6d6daff74efbd35c565cf1b70
Tina Thai <tinathai05@gmail.com>
2024-07-01 18:43:51 -0400

    final

---
+++ send_email.php
@@ -0,0 +1,26 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $to = "your_email@example.com";
+    $subject = "New Message from Contact Form";
+
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message:\n$message\n";
+
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $email_content, $headers)) {
+        echo "Message sent successfully.";
+    } else {
+        echo "Oops! Something went wrong. Please try again later.";
+    }
+} else {
+    header("Location: index.html");
+    exit;
+}
+?>
