d1c406e0ed50f65107c993171a38133650340d55
Tina Thai <tinathai05@gmail.com>
2025-11-06 19:14:35 -0500

    swap projects and prof dev

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ function App() {
       <Hero />
       <Education />
       <Experience />
-      <Projects />
       <ProfessionalDev />
+      <Projects />
       <Volunteer />
       <Certifications />
       <ContactForm />
